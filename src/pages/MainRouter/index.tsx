fde0f82fbd008366c9005f0d2de4c130128b906d
Shadowrunner11 <matematicadivina1@gmail.com>
2022-08-30 12:04:07 -0500

    :bug: routing base

--- src/pages/MainRouter/index.tsx
+++ src/pages/MainRouter/index.tsx
@@ -5,7 +5,7 @@ import Register from '../Register';
 
 const MainRouter = ()=>{
   return(
-    <BrowserRouter>
+    <BrowserRouter basename='/tzuzulCodeReact'>
       <Nav />
       <Routes>
         <Route element={<Register />} path='/singup' />
